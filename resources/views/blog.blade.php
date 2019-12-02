20220c7ab2600aeffa5f6c2063ce1e0b8f498877
dilfas <18523054@students.uii.ac.id>
2019-12-02 16:12:25 +0700

    dilfo

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Tutorial Laravel</title>
+</head>
+<body>
+    
+    <h3>www.malasngoding.com</h3>
+    <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+    <p>Ini adalah view blog. ada di route blog.</p>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function(){
+    return "Hallo, Selamat datang di tutorial laravel www.malascoding.com";
+});
+
+Route::get('blog', function(){
+    return view('blog');
+});
+
+Route::get('dosen', 'DosenController@index');
